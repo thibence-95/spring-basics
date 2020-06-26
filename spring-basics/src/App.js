aea6d778c367d474fe334ea750f13a002f826534
Thibaut Salence <salencethibaut95@gmail.com>
2020-06-26 15:11:10 +0200

    just checking

--- spring-basics/src/App.js
+++ spring-basics/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import {useSpring, animated} from 'react-spring';
 import './App.css';
+import './components/intro/intro.js';
+
 
 function App() {
+  return (
+    <div className="App">
 
+    </div>
+  );
 }
-//export default App;
+
+export default App;
---
+++ spring-basics/src/components/intro/intro.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom"
+
+import './intro.css';
+
+class intro extends Component {
+render() {
+    return <div>
+        <h1>Hi. I'm Thibaut</h1>
+        <h3>Welcome to my website</h3>
+    </div>
+    }
+}
+
+export default intro;
